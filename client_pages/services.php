039442b33934fdf28d20522cfcd5983b84b97bf8
155972 <155972@student.uwm.edu.pl>
2022-01-20 21:07:14 +0100

    cosmetically changes

---
+++ client_pages/services.php
@@ -0,0 +1,91 @@
+<style>
+    #services{
+        background-color: #e0a4aa;
+        border-radius: 100px;
+    }
+</style>
+
+<section>
+<div class="container">
+
+<h3 class="service_name">Konsultacja detektywistyczna</h3>
+<table>
+    <tr>
+        <td style="width: 85%;">
+            <p>
+            Umów się na bezpłatną konsultację telefoniczną i opowiedz o swojej sytuacji. Dopasujemy się do Twoich możliwości i znajdziemy najlepsze rozwiązanie.
+            </p>
+        </td>
+        <td class="t_check_price">
+            <button class="b_check_price">sprawdź cenę</button>
+        </td>
+    </tr>
+</table>
+
+</div>
+
+<div class="container">
+<h3 class="service_name">Badanie wariografem (wykrywacz kłamstw)</h3>
+<table>
+    <tr>
+        <td style="width: 85%;">
+            <p>
+            Istnieją sytuacje, w których dotarcie do prawdy jest niezwykle trudne. Dotyczy to zarówno życia prywatnego, jak i zawodowego. Właśnie dlatego tak dużo osób decyduje się podpiąć pod wariograf. Cena w porównaniu do korzyści, jakie można zyskać, jest naprawdę przystępna. Należy podkreślić, że badanie wariografem ukazuje prawdę, a nieraz pozwala oczyścić się z zarzutów.
+            </p>
+        </td>
+        <td class="t_check_price">
+            <button class="b_check_price">sprawdź cenę</button>
+        </td>
+    </tr>
+</table>
+</div>
+
+<div class="container">
+<h3 class="service_name">Obserwacja osoby</h3>
+<table>
+    <tr>
+        <td style="width: 85%;">
+            <p>
+            Udowodnienie komuś bliskiemu nielojalności lub zdrady nie jest sprawą łatwą. Dla tych, którzy chcą dojść prawdy, śledzenie na własną rękę męża lub żony jest dodatkowo bardzo obciążające psychicznie i emocjonalnie. Dlatego obserwację osób, względem których żywimy silne uczucia i równie silne podejrzenia warto powierzyć profesjonalistom.
+            </p>
+        </td>
+        <td class="t_check_price">
+            <button class="b_check_price">sprawdź cenę</button>
+        </td>
+    </tr>
+</table>
+</div>
+
+<div class="container">
+<h3 class="service_name">Ustalenie numeru PESEL</h3>
+<table>
+    <tr>
+        <td style="width: 85%;">
+            <p>
+            Wymagane dodatkowe informacje (np. imiona rodziców, miejsce urodzenia, adres zameldowania).
+            </p>
+        </td>
+        <td class="t_check_price">
+            <button class="b_check_price">sprawdź cenę</button>
+        </td>
+    </tr>
+</table>
+</div>
+
+<div class="container">
+<h3 class="service_name">Poszukiwanie osób zaginionych</h3>
+<table>
+    <tr>
+        <td style="width: 85%;">
+            <p>
+            Każdego roku policja odnotowuje kilkanaście tysięcy zaginięć. Każdego z nas może spotkać taka sytuacja, gdy ktoś z rodziny, znajomych zaginie z dnia na dzień. Zaginąć może każdy niezależnie od płci, wieku, wykształcenia. Najważniejsze, by w takich sytuacjach zacząć jak najszybciej działać.
+            </p>
+        </td>
+        <td class="t_check_price">
+            <button class="b_check_price">sprawdź cenę</button>
+        </td>
+    </tr>
+</table>
+</div>
+
+</section>
