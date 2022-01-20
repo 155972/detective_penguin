30647db304b991ab30b4aebef57a62160529efc1
Igor Boradyn <boradyn555@gmail.com>
2022-01-20 22:56:38 +0100

    kurła, znowu psujo

--- logCHCK.php
+++ logCHCK.php
@@ -1,17 +1,17 @@
 <?php
     session_start();
-    
+
 	if (isset($_SESSION['log_in']) && $_SESSION['log_in'])
 		header('Location: index.php');
-	
+
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		require_once "connect.php";
-		
+
 		$connect = @new mysqli($host, $db_user, $db_password, $db_name);
-		
+
 		$connect->query('SET NAMES utf8');
 		$connect->query('SET CHARACTER_SET utf8_unicode_ci');
-		
+
 		if ($connect->connect_errno!=0) {
 			echo "Error: ".$connect->connect_errno;
 		}
@@ -19,20 +19,22 @@
             $email = $_POST['email'];
             $pass = $_POST['password'];
             $rows_email = $connect->query("SELECT email FROM user WHERE email='$email'")->num_rows;
-            
+
             if ($rows_email<1) header('Location: log.php?bdlgin=1');
-        
+
             $user = $connect->query("SELECT * FROM user WHERE email='$email'")->fetch_assoc();
-            
+
             if ($user['passwd'] == $pass) {
+                $uID = $user['ID'];
                 $_SESSION['log_in'] = true;
-                $_SESSION['UserID'] = $user['ID'];
+                $_SESSION['UserID'] = $uID;
                 $_SESSION['Email'] = $user['email'];
                 $_SESSION['FName'] = $user['first_name'];
                 $_SESSION['LName'] = $user['last_name'];
                 $_SESSION['Phone'] = $user['phone'];
-                
-                $connect->query("UPDATE user WHERE email='$email'");
+
+                $connect->query("UPDATE user SET last_accessed = CURRENT_DATE() WHERE ID = '$uID'");
+
                 header('Location: index.php');
             }
             header('Location: log.php?bdlgin=1');
