4292ac4dc678771091eddc62721934e281e86637
155972 <155972@student.uwm.edu.pl>
2022-01-22 00:45:53 +0100

    add employee's content - still in progress

---
+++ empl/account.php
@@ -0,0 +1,41 @@
+<style>
+    #account{
+        background-image: linear-gradient(144deg, rgba(244, 180, 255, 0.6), rgba(146, 136, 215, 0.6));
+        border-radius: 100px;
+    }
+</style>
+
+<div class="container">
+    <table width="50%" border=0>
+        <tr>
+            <td>Imię: </td>
+            <td style="text-align: right;">Jan</td>
+        </tr>
+        <tr>
+            <td>Nazwisko: </td>
+            <td style="text-align: right;">Talerz</td>
+        </tr>
+        <tr>
+            <td>PESEL: </td>
+            <td style="text-align: right;">91042156481</td>
+        </tr>
+        <tr>
+            <td>Nr Tel: </td>
+            <td style="text-align: right;">600821732</td>
+        </tr>
+        <tr>
+            <td>E-mail: </td>
+            <td style="text-align: right;">jtalerz@gmail.com</td>
+        </tr>
+        <tr>
+            <td>Nr licencji: </td>
+            <td style="text-align: right;">0003621</td>
+        </tr>
+        <tr>
+            <td colspan=2></td>
+        </tr>
+        <tr>
+            <td colspan=2><button class="b_check_price" onclick="contentChange('')">Edytuj dane</button></td>
+        </tr>
+    </table>
+</div>
