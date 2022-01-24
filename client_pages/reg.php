d86959b9c4da99222fab514fdf43f0516cc72476
Igor Boradyn <boradyn555@gmail.com>
2022-01-24 06:10:40 +0100

    Poprawione odwołanie do index.php

--- client_pages/reg.php
+++ client_pages/reg.php
@@ -2,7 +2,7 @@
     session_start(); 
         
     if (isset($_SESSION['log_in']) && $_SESSION['log_in'])
-        header('Location: index.php');
+        header('Location: ../index.php');
 ?>
 
 <!DOCTYPE html>
