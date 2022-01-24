963e1a037ae63e779182ecc87a7b812fab794366
Igor Boradyn <boradyn555@gmail.com>
2022-01-24 04:37:00 +0100

    przekierowanie do odpowiedniej podstrony w zależności od tego, kto jest zalogowany

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-    header("Location: client_pages/home.html");
+    session_start();
+    require_once "connect.php";
+
+    $connect = @new mysqli($host, $db_user, $db_password, $db_name);
+
+    $connect->set_charset('utf8');
+
+    if ($connect->connect_errno!=0) {
+        echo "Error: ".$connect->connect_errno;
+    }
+    else { 
+        if(!(isset($_SESSION['log_in']) && $_SESSION['log_in']))
+            header("Location: client_pages/home.html");
+        else{
+            $uID = $_SESSION['UserID'];
+            $client = $connect->query("SELECT ID FROM client WHERE ID_user='$uID'")->num_rows;
+    
+            if($client >= 1)
+                header("Location: client_pages/home.html");
+            else
+                header("Location: empl/account.php");
+        }
+    }
 ?>
