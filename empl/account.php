20b12fd68c99c8fc75ff3f0f716e38a912501206
Igor Boradyn <boradyn555@gmail.com>
2022-01-24 07:09:35 +0100

    Działające odczytywanie i edytowanie danych pracownika

--- empl/account.php
+++ empl/account.php
@@ -1,3 +1,40 @@
+<?php
+    session_start();
+	if(!(isset($_SESSION['log_in']) && $_SESSION['log_in']))
+		header('Location: ../index.php');
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        require_once "../connect.php";
+
+        $connect = @new mysqli($host, $db_user, $db_password, $db_name);
+
+        $connect->set_charset('utf8');
+
+        if ($connect->connect_errno!=0) {
+            echo "Error: ".$connect->connect_errno;
+        }
+        else {
+            $uID = $_SESSION['userID'];
+            $fName = $_POST['firstname'];
+            $lName = $_POST['surname'];
+            $pesel = $_POST['PESEL'];
+            $phone = $_POST['phonenumber'];
+            $email = $_POST['email'];
+            $licence = $_POST['licence'];
+
+            $_SESSION['fName'] = $_POST['firstname'];
+            $_SESSION['lName'] = $_POST['surname'];
+            $_SESSION['PESEL'] = $_POST['PESEL'];
+            $_SESSION['phone'] = $_POST['phonenumber'];
+            $_SESSION['email'] = $_POST['email'];
+            $_SESSION['licence_number'] = $_POST['licence'];
+
+            $connect->query("UPDATE `user` SET `first_name` = '$fName', `last_name` = '$lName', `phone` = '$phone' WHERE `user`.`ID` = '$uID'");
+            $connect->query("UPDATE employee SET PESEL = '$pesel', licence_number = '$licence' WHERE ID_user = '$uID'");
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -28,27 +65,27 @@
         <table width="30%">
             <tr>
                 <td>Imię: </td>
-                <td style="text-align: right;">Jan</td>
+                <td style="text-align: right;"><?php echo $_SESSION['fName'] ?></td>
             </tr>
             <tr>
                 <td>Nazwisko: </td>
-                <td style="text-align: right;">Talerz</td>
+                <td style="text-align: right;"><?php echo $_SESSION['lName'] ?></td>
             </tr>
             <tr>
                 <td>PESEL: </td>
-                <td style="text-align: right;">91042156481</td>
+                <td style="text-align: right;"><?php echo $_SESSION['PESEL'] ?></td>
             </tr>
             <tr>
                 <td>Nr Tel: </td>
-                <td style="text-align: right;">600821732</td>
+                <td style="text-align: right;"><?php echo $_SESSION['phone'] ?></td>
             </tr>
             <tr>
                 <td>E-mail: </td>
-                <td style="text-align: right;">jtalerz@gmail.com</td>
+                <td style="text-align: right;"><?php echo $_SESSION['email'] ?></td>
             </tr>
             <tr>
                 <td>Nr licencji: </td>
-                <td style="text-align: right;">0003621</td>
+                <td style="text-align: right;"><?php echo $_SESSION['licence_number'] ?></td>
             </tr>
             <tr>
                 <td colspan=2><button class="b_check_price" onclick="$('.right').load('edit_empl.php');">Edytuj dane</button></td>
--- empl/edit_empl.php
+++ empl/edit_empl.php
@@ -1,32 +1,38 @@
-<form>
+<?php
+    session_start();
+	if(!(isset($_SESSION['log_in']) && $_SESSION['log_in']))
+		header('Location: ../index.php');
+?>
+
+<form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>">
     <table width="30%">
         <tr>
             <td><label for="firstname">Imię: </label></td>
-            <td style="text-align: right;"><input type="text" name="firstname" value="Jan"></td>
+            <td style="text-align: right;"><input type="text" name="firstname" value="<?php echo $_SESSION['fName'] ?>"></td>
         </tr>
         <tr>
             <td><label for="surname">Nazwisko: </label></td>
-            <td style="text-align: right;"><input type="text" name="surname" value="Talerz"></td>
+            <td style="text-align: right;"><input type="text" name="surname" value="<?php echo $_SESSION['lName'] ?>"></td>
         </tr>
         <tr>
             <td><label for="PESEL">PESEL: </label></td>
-            <td style="text-align: right;"><input type="text" name="PESEL" value="91042156481"></td>
+            <td style="text-align: right;"><input type="text" name="PESEL" value="<?php echo $_SESSION['PESEL'] ?>"></td>
         </tr>
         <tr>
             <td><label for="phonenumber">Nr Tel: </label></td>
-            <td style="text-align: right;"><input type="text" name="phonenumber" value="600821732"></td>
+            <td style="text-align: right;"><input type="text" name="phonenumber" value="<?php echo $_SESSION['phone'] ?>"></td>
         </tr>
         <tr>
             <td><label for="email">E-mail: </label></td>
-            <td style="text-align: right;"><input type="text" name="email" value="jtalerz@gmail.com"></td>
+            <td style="text-align: right;"><input type="text" name="email" value="<?php echo $_SESSION['email'] ?>"></td>
         </tr>
         <tr>
             <td><label for="licence">Nr licencji: </label></td>
-            <td style="text-align: right;"><input type="text" name="licence" value="0003621"></td>
+            <td style="text-align: right;"><input type="text" name="licence" value="<?php echo $_SESSION['licence_number'] ?>"></td>
         </tr>
         <tr>
             <td style="float:left"><a href="account.php"><button class="b_check_price">Powrót</button></a></td>
-            <td><input type=submit class="b_check_price" onclick="$('.right').load('account.php');" value="Zapisz dane"></td>
+            <td><input type=submit class="b_check_price" value="Zapisz dane"></td>
         </tr>
     </table>
 </form>
